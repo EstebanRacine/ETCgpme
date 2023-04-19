95243749bac0d0cec27b82eceaf16be0b69d58c7
EstebanRacine <esteban.racineecole@gmail.com>
2023-04-19 14:52:24 +0200

    Page Accueil

--- accueil.php
+++ accueil.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/accueil.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="shortcut icon" href="images/logoETC.png" type="image/x-icon">
     <title>Accueil</title>
@@ -17,7 +18,10 @@
     ?>
 
     <div class="contenuAccueil">
-
+        <h1>ETC</h1>
+        <h2 class="enroule">ENROULE TON CABLE</h2>
+        <h2 class="slogan">C'EST IMPE'CABLE</h2>
+        <img src="images/accueil/imgAccueil.jpg" alt="Image de produits">
     </div>
 
     <?php
--- fichiersCommuns/jsNav.js
+++ fichiersCommuns/jsNav.js
@@ -1,5 +1,6 @@
 let btnIcon = document.getElementById('contenuBoutonNav'),
     btn = document.getElementById('affichageNavTel'),
+    body = document.body,
     nav = document.getElementById('Menu');
 
 const apparitionMenu = () => {
@@ -8,15 +9,18 @@ const apparitionMenu = () => {
         btnIcon.style.color = "#1c9aea";
         btnIcon.className = "fa-solid fa-bars";
         nav.style.marginLeft = "100vw";
+        body.style.height = "max-content";
+        body.style.overflowY = "auto"
     }else{
         btnIcon.style.color = "white";
         nav.style.marginLeft = "0";
-        btnIcon.className = "fa-solid fa-xmark"
+        btnIcon.className = "fa-solid fa-xmark";
+        body.style.height = "100vh";
+        body.style.overflowY = "hidden"
     }
 }
 
 const modifSizeNav = () =>{
-    // console.log(window.innerWidth);
     if (window.innerWidth >= 800  && btnIcon.style.color === "#1c9aea"){
         console.log(36);
         nav.style.marginLeft = "0";
