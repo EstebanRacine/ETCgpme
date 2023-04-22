735dbd992278ba031ef2c84067e5edc574a01396
EstebanRacine <esteban.racineecole@gmail.com>
2023-04-22 19:03:12 +0200

    Page Contact

--- contact.php
+++ contact.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/contact.css">
     <link rel="shortcut icon" href="images/logoETC.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Qui Sommes-Nous ?</title>
@@ -18,7 +19,23 @@
 
     <div class="contenuContact">
 
+        <h1 class="TitreTel">Nous Contacter</h1>
+
+        <img id="imageDecorContact" src="images/contact/imgContact.jpg" alt="Image">
+        <div class="reseaux">
+            <h2>Nos Réseaux</h2>
+            <a href="https://www.instagram.com/etcgpme/" target="_blank"><i class="fa-brands fa-instagram"></i> @etcgpme </a>
+            <a href="https://www.tiktok.com/@etcgpgme" target="_blank"><i class="fa-brands fa-tiktok"></i> @etcgpme</a>
+            <a href="https://www.facebook.com/profile.php?id=100091503001565" target="_blank"><i class="fa-brands fa-facebook-f"></i> ETC GPME </a>
+            <a target="_blank" href="https://www.linkedin.com/in/b%C3%BC%C5%9Fra-polat-29004426a/?originalSubdomain=fr"><i class="fa-brands fa-linkedin-in"></i> Büşra POLAT </a>
+        </div>
+        <div class="blocAbsContact">
+            <h1 class="titreContact">Nous Contacter</h1>
+            <h1 class="TitreTel contactMail">Envoyez nous un email</h1>
+            <a href="mailto:etc.gpme@gmail.com" onmouseover="document.getElementById('gifEnveloppe').src='images/contact/mail-mouv.gif'" onmouseout="document.getElementById('gifEnveloppe').src='images/contact/mail-static.png'"><img id="gifEnveloppe" src="images/contact/mail-static.png" alt="Image d'enveloppe" >etc.gpme@gmail.com</a>
+        </div>
     </div>
+
 
     <?php
     include "fichiersCommuns/footer.php";
