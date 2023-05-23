6437c43e6905c5bad219e3522907b7ac015cccd0
Esteban Racine <esteban.racineecole@gmail.com>
2023-05-23 08:15:22 +0200

    modif mail

--- index.php
+++ index.php
@@ -1,2 +1,47 @@
-<?php
-header('Location: accueil.php');
+<!doctype html>
+<html lang="fr">
+<head>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CJDWK8C36Z"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-CJDWK8C36Z');
+    </script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/accueil.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="shortcut icon" href="images/logoETC2.png" type="image/x-icon">
+    <title>ETC - Enroule Ton Câble</title>
+</head>
+<body>
+<div class="container">
+    <?php
+    include "fichiersCommuns/header.php";
+    ?>
+
+    <div class="contenuAccueil">
+        <h1 class="welcome">Bienvenue sur</h1>
+        <h1 class="ETCName">ETC</h1>
+        <h2 class="enroule">ENROULE TON CABLE</h2>
+        <h2 class="slogan">C'EST IMPE'CABLE</h2>
+        <img src="images/accueil/imgAccueil.jpg" alt="Image de produits">
+    </div>
+
+    <?php
+    include "fichiersCommuns/footer.php";
+    ?>
+</div>
+
+
+
+</body>
+</html>
